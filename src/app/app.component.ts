c4f4bb6649023767fefa16474ae0c4b2d2d91326
saddamhr <saddambubt65@gmail.com>
2020-11-27 21:46:30 +0600

    component templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'udemy-angular-the-Complete-guide';
-  name = "Saddam Hossain"
 }
