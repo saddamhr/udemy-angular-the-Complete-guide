815ce59b583a9a315450479115b39053979b046e
saddamhr <saddambubt65@gmail.com>
2020-12-20 17:31:17 +0600

    module 10 done

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,38 @@
-import { EventEmitter } from '@angular/core';
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
    private recipes:Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21-768x1152.jpg'),
-        new Recipe('An Test Recipe', 'This is simply a test', 'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21-768x1152.jpg')
+        new Recipe(
+            'A Test Recipe',
+             'This is simply a test',
+              'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21-768x1152.jpg',
+               [
+                   new Ingredient('Meat', 1),
+                   new Ingredient('French Fries', 20)
+               ]),
+        new Recipe(
+            'An Test Recipe',
+         'This is simply a test',
+          'https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21-768x1152.jpg',
+           [
+               new Ingredient('Buns', 2),
+               new Ingredient('Meat', 1)
+           ])
       ];
+
+      constructor(private slService: ShoppingListService) {}
 
       getRecipes() {
           return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+          this.slService.addIngredients(ingredients)
       }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { ShoppingListService } from './shopping-list.service';
 import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from './../shared/ingredient.model';
@@ -7,19 +8,18 @@ import { Ingredient } from './../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
+  ingredients: Ingredient[] = [];
 
-  ];
-
-  constructor() { }
+  constructor(private slService: ShoppingListService ) { }
 
   ngOnInit(): void {
-  }
-
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+    this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientChanged
+    .subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    )
   }
 
 }
